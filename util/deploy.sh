a64f9bfa3e25f5e2d35aab696a8ab3e75b55dcd6
Ricky Hussmann <ricky.hussmann@gmail.com>
2017-10-02 07:56:16 -0400

    Make deploy script idempotent

--- util/deploy.sh
+++ util/deploy.sh
@@ -5,4 +5,6 @@ npm install
 aws s3 mb s3://form.asyouareboudoir.com/
 aws s3 website s3://form.asyouareboudoir.com/ \
     --index-document index.html --error-document error.html
+aws s3api put-bucket-policy --bucket form.asyouareboudoir.com \
+    --policy file://${PWD}/util/policy.json
 aws s3 sync ./build s3://form.asyouareboudoir.com/
