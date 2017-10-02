a080609e6914f9dc5d2f1158e750d0b5a82ee761
Ricky Hussmann <ricky.hussmann@gmail.com>
2017-10-02 07:45:00 -0400

    Access form.asyouareboudoir.com as a website

--- util/deploy.sh
+++ util/deploy.sh
@@ -3,4 +3,6 @@ set -xe
 npm install
 ./node_modules/grunt-cli/bin/grunt
 aws s3 mb s3://form.asyouareboudoir.com/
+aws s3 website s3://form.asyouareboudoir.com/ \
+    --index-document index.html --error-document error.html
 aws s3 sync ./build s3://form.asyouareboudoir.com/
