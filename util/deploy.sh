f03aaa55f26c38c684b6e6989ac6972b44ab603e
Ricky Hussmann <ricky.hussmann@gmail.com>
2017-10-02 21:43:36 -0400

    Copy `basic.mustache` into the deployed assets.

--- util/deploy.sh
+++ util/deploy.sh
@@ -7,4 +7,6 @@ aws s3 website s3://form.asyouareboudoir.com/ \
     --index-document index.html --error-document error.html
 aws s3api put-bucket-policy --bucket form.asyouareboudoir.com \
     --policy file://${PWD}/util/policy.json
+
 aws s3 sync ./build s3://form.asyouareboudoir.com/
+aws s3 cp ./templates/basic.mustache s3://form.asyouareboudoir.com/
